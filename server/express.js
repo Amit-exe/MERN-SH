a7136a6d9b48b228acf7d654e8276a4424855da0
Amit-exe <amit.kushwaha.exe@gmail.com>
2025-02-09 02:11:06 +0530

    basic node backend with user crud and auth error handling mongo(template)

--- server/express.js
+++ server/express.js
@@ -1,21 +1,38 @@
 import express from "express";
+import path from "path";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compress from "compression";
 import cors from "cors";
 import helmet from "helmet";
 
-const app = express();
-app.use((req, res, next) => {
-  console.log(req.path, req.method);
-  next();
-});
+import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 
+const app = express();
+
+// parse body params and attache them to req.body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(compress());
+// secure apps by setting various HTTP headers
 app.use(helmet());
+// enable CORS - Cross Origin Resource Sharing
 app.use(cors());
+
+// mount routes
+app.use("/", userRoutes);
+app.use("/", authRoutes);
+
+// Catch unauthorised errors
+app.use((err, req, res, next) => {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({ error: err.name + ": " + err.message });
+  } else if (err) {
+    res.status(400).json({ error: err.name + ": " + err.message });
+    console.log(err);
+  }
+});
 
 export default app;
